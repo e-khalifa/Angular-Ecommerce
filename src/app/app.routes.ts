15a77e7055e0ab251ed732834ca18caf13956ed2
Omar9Tarek <Omar9Tarek@gmail.com>
2025-03-11 03:23:39 +0200

    Added Cart structure, Checkout coming soon ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './features/home/home.component';
 import { CardComponent } from './features/card/card.component';
 import { AboutComponent } from './features/about/about.component';
 import { ContactUsComponent } from './features/contact-us/contact-us.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
     { path: "login", component: LoginComponent, title: "Login" },
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: "card", component: CardComponent, title: "Card" },
     { path: "about", component: AboutComponent, title: "About" },
     { path: "contactus", component: ContactUsComponent, title: "Contact Us" },
-
+    {path:  "checkout", component:CheckoutComponent,    title:"Checkout"},
+    
     { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
