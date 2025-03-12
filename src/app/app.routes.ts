67dbb34331c62d250b64085b9164cbcb8f880bb9
yousefhamdy3108 <yousef.hamdy3108@gmail.com>
2025-03-12 18:58:14 +0200

    add auth guard to check user login or not

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,12 +10,21 @@ import { OrdersComponent } from './features/profile/orders/orders.component';
 import { SettingsComponent } from './features/profile/settings/settings.component';
 import { PasswordComponent } from './features/profile/password/password.component';
 import { CheckoutComponent } from './features/checkout/checkout.component';
+import { AuthGuard } from './features/auth/auth.guard';
 
 export const routes: Routes = [
-  // { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'register', component: RegisterComponent, title: 'Register' },
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  { path: 'card', component: CardComponent, title: 'Card' },
+  { path: 'about', component: AboutComponent, title: 'About' },
+  { path: 'contactus', component: ContactUsComponent, title: 'Contact Us' },
+  { path: 'checkout', component: CheckoutComponent, title: 'Checkout' },
+
   {
-    path: 'login',
+    path: 'profile',
     component: ProfileComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: 'orders', component: OrdersComponent },
       { path: 'passwords', component: PasswordComponent },
@@ -23,12 +32,6 @@ export const routes: Routes = [
       { path: '', redirectTo: 'settings', pathMatch: 'full' },
     ],
   },
-  { path: 'register', component: RegisterComponent, title: 'Register' },
-  { path: 'home', component: HomeComponent, title: 'Home' },
-  { path: 'card', component: CardComponent, title: 'Card' },
-  { path: 'about', component: AboutComponent, title: 'About' },
-  { path: 'contactus', component: ContactUsComponent, title: 'Contact Us' },
-  { path: 'checkout', component: CheckoutComponent, title: 'Checkout' },
 
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { UsersService } from '../../../services/users.service';
 
@@ -7,48 +12,67 @@ import { UsersService } from '../../../services/users.service';
   selector: 'app-login',
   imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
   usersData: any;
-  constructor(private myUserService: UsersService, private router: Router) { }
+
+  constructor(private myUserService: UsersService, private router: Router) {}
+
   ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.router.navigate(['/profile']);
+      return;
+    }
+
     this.myUserService.getAllUsers().subscribe({
       next: (data) => {
         console.log(data);
-        this.usersData = data
+        this.usersData = data;
       },
-      error: (err) => { console.log(err) }
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
 
   loginForm = new FormGroup({
     email: new FormControl(null, [Validators.email, Validators.required]),
-    password: new FormControl(null,
-      [Validators.required,
+    password: new FormControl(null, [
+      Validators.required,
       Validators.minLength(8),
-      Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
-      ])
-  }
-  )
+      Validators.pattern(
+        /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/
+      ),
+    ]),
+  });
+
   get email() {
     return this.loginForm.get('email');
   }
+
   get password() {
     return this.loginForm.get('password');
   }
+
   onSubmit() {
     if (this.loginForm.valid) {
-      const user = this.usersData.find((u: any) => u.email === this.email?.value);
+      const user = this.usersData.find(
+        (u: any) => u.email === this.email?.value
+      );
       if (!user) {
         this.loginForm.controls['email'].setErrors({ notFound: true });
       } else if (user.password !== this.password?.value) {
-        this.loginForm.controls['password'].setErrors({ incorrectPassword: true });
+        this.loginForm.controls['password'].setErrors({
+          incorrectPassword: true,
+        });
       } else {
-        localStorage.setItem('userId', user.id);
+        const token = btoa(user.id);
+        localStorage.setItem('token', token);
+
         this.router.navigate(['/home']);
       }
     }
   }
 }
-
