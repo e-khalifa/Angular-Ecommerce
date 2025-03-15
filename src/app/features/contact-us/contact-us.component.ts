fd763675af70c9edabed58ef63fe90b20fb8fd48
VerinaMaged <101900193+VerinaMaged@users.noreply.github.com>
2025-03-15 02:09:57 +0200

    uppdate Contact us

--- src/app/features/contact-us/contact-us.component.ts
+++ src/app/features/contact-us/contact-us.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styles: ``
 })
 export class ContactUsComponent {
 
+  contactForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.contactForm.valid) {
+      console.log('Form Data:', this.contactForm.value);
+      alert('Message sent successfully!');
+      this.contactForm.reset();
+      this.submitted = false;
+    }
+  }
 }
