afc1da2de5023be2d874fb4f840248c68ff0417a
Omar9Tarek <Omar9Tarek@gmail.com>
2025-03-20 03:11:41 +0200

    added paypal gateway (not completely finished yet)

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-checkout',
   imports: [RouterModule,CommonModule],
   templateUrl: './checkout.component.html',
   styles: ``
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit{
+  @ViewChild('paymentRef', { static: true }) paymentRef!: ElementRef;
+ 
   constructor(private myCart:CartService){
     this.cart=myCart.getCart();
 
@@ -23,7 +26,10 @@ export class CheckoutComponent {
       this.total = this.myCart.getTotal(); 
     });
 
+    console.log(window.paypal);
     this.total = this.myCart.getTotal();
+    window.paypal.Buttons().render(this.paymentRef.nativeElement);
+
   }
 
 
